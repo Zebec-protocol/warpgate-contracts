12308371f7fd6a1e26556287511047efe8b861fc
asmitbhantana <bhantanasmit@gmail.com>
2023-11-21 18:03:36 +0545

    Launchpad contracts compile

---
+++ projects/launchpad/hardhat.config.ts
@@ -0,0 +1,58 @@
+import type { HardhatUserConfig, NetworkUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-truffle5";
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import "solidity-coverage";
+import "dotenv/config";
+
+const bscTestnet: NetworkUserConfig = {
+  url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  chainId: 97,
+  accounts: [process.env.KEY_TESTNET!],
+};
+
+const bscMainnet: NetworkUserConfig = {
+  url: "https://bsc-dataseed.binance.org/",
+  chainId: 56,
+  accounts: [process.env.KEY_MAINNET!],
+};
+
+const sepolia: NetworkUserConfig = {
+  url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+  chainId: 11155111,
+  accounts: [process.env.PRIVATE_KEY!],
+};
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      gas: 120000000,
+      blockGasLimit: 0x1fffffffffffff,
+    },
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 99999,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  abiExporter: {
+    path: "./data/abi",
+    clear: true,
+    flat: false,
+  },
+};
+
+export default config;
